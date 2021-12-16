b3f393d87322b75e4cde8f77e1de808be5655e10
krayemnada <nadakrayem41@gmail.com>
2021-12-16 22:15:21 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,87 @@
-import React, { Component } from 'react'
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+    state = {
+        value: "",
+        data: [
+            { Task: "React", id: 1, complete: false },
+            { Task: "React props", id: 2, complete: false },
+            { Task: "React state", id: 3, complete: false },
+        ],
+    };
 
-export class App extends Component {
-  state ={
-    
-    Task1 : "React",
-    Task2: "React props",
-    Task3 : "React state",
-  
-  }
-  handleAdd = () =>{
-    this.setState({ Add:! this.state.Add});
-  }
-  render() {
-    return (
-      <div>
-        <h4> {this.state.Task1} </h4>
-        <h4> {this.state.Task2} </h4>
-        <h4> {this.state.Task3} </h4>
-        
-        <form onSubmit={this.handleAdd}>
-        <label>
-          Nom :
-          <input type="text" value={this.state.value}  />
-        </label>
-        <input type="submit" value="Envoyer" />
-      </form>
-      </div>
-    )
-  }
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    };
+
+    handleSubmit = (event) => {
+        if (this.state.value !== "") {
+            this.setState({
+                data: [
+                    ...this.state.data,
+                    {
+                        Task: this.state.value,
+                        id: Math.random() * Math.pow(10, 17),
+                        complete: false,
+                    },
+                ],
+            });
+            this.setState({ value: "" });
+        }
+        event.preventDefault();
+    };
+    handleDelete = (id) => {
+        this.setState({
+            data: this.state.data.filter((el) => el.id !== id),
+        });
+    };
+    handleDone = (id) => {
+        this.setState({
+            data: this.state.data.map((task) =>
+                task.id === id ? { ...task, complete: !task.complete } : task
+            ),
+        });
+    };
+
+    render() {
+        return (
+            <div className="List">
+                <h1 style={{color : "pink"}}>ToDo List</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Add Task :</label>
+                    <input
+                        type="text"
+                        value={this.state.value}
+                        onChange={this.handleChange}
+                    />
+
+                    <input type="submit" value="Add" />
+                </form>
+                {this.state.data.map((task) => (
+                    <div key={task.id} className="task">
+                        <h4 className={task.complete ? "done" : "notDone"}>
+                            {task.Task}
+                        </h4>
+
+                        <button
+                            className={
+                                task.complete ? "btn-done" : "btn-notDone"
+                            }
+                            onClick={this.handleDone.bind(this, task.id)}
+                        >
+                            {!task.complete ? "Done" : "Not Done"}
+                        </button>
+                        <button
+                            class="btn-delete"
+                            onClick={this.handleDelete.bind(this, task.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 
-export default App
-
+export default App;
